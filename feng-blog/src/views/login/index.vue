cc48af9188785e45aec0903592698bbf8437db8b
ShimlyNa <1432979195@qq.com>
2023-12-12 11:10:25 +0800

    ...

--- feng-blog/src/views/login/index.vue
+++ feng-blog/src/views/login/index.vue
@@ -34,242 +34,128 @@ export default {
 </script>
 
 <template>
-  <div class="mi-login">
-    <div class="mi-login__main">
-      <div :class="['mi-login-container', 'container-login',{ 'is-txl is-z200': isLogin }] ">
-        <!-- 登录表  -->
-        <el-form
-            :model="loginForm"
-            v-if="$route.name == 'feng-login'"
-            name=""
-            class="login-register--form"
-        >
+  <div class="mi-userform">
+    <div class="mi-userform__container right-panel-active">
+      <div class="mi-userform__containe--form container-login">
+        <!-- 登陆表 -->
+        <el-form :model="loginForm" class="login__form">
           <el-form-item>
-            <h1 class="loginTitle">LOGIN</h1>
+            <h1 class="form__title">LOGIN</h1>
           </el-form-item>
           <el-form-item>
-            <el-input v-model="loginForm.username" placeholder="Username" clearable autocomplete="false">
-              <template #prefix>
-                <el-icon><UserFilled /></el-icon>
-              </template>
-            </el-input>
+            <el-input v-model="loginForm.username" placeholder="Username" autocomplete="off" clearable></el-input>
           </el-form-item>
           <el-form-item>
-            <el-input v-model="loginForm.password" placeholder="Username" show-password autocomplete="false">
-              <template #prefix>
-                <el-icon><Lock /></el-icon>
-              </template>
-            </el-input>
+            <el-input v-model="loginForm.password" placeholder="Password" autocomplete="off" show-password></el-input>
           </el-form-item>
           <el-form-item>
             <el-form-item>
-              <el-checkbox v-model="loginForm.remember">记住密码</el-checkbox>
+              <el-checkbox v-model="loginForm.remember">记住我</el-checkbox>
             </el-form-item>
-            <a class="login-register-form-forgo" href="" >忘记密码</a>
+            <a class="">忘记密码</a>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" class="formBtn login-register-form-button">登录</el-button>
-          </el-form-item>
-        </el-form>
-        <!-- 登录表  -->
-        <el-form
-            :model="registerForm"
-            v-else
-            name=""
-            class="login-register--form"
-        >
-          <el-form-item>
-            <h1 class="loginTitle">Register</h1>
-          </el-form-item>
-          <el-form-item>
-            <el-input v-model="registerForm.username" placeholder="Username" clearable autocomplete="false">
-              <template #prefix>
-                <el-icon><UserFilled /></el-icon>
-              </template>
-            </el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-input v-model="registerForm.password" placeholder="Password" show-password autocomplete="false">
-              <template #prefix>
-                <el-icon><Lock /></el-icon>
-              </template>
-            </el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-input v-model="registerForm.check_password" placeholder="Password" show-password autocomplete="false">
-              <template #prefix>
-                <el-icon><Lock /></el-icon>
-              </template>
-            </el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-input v-model="registerForm.nick_name" placeholder="NickName" show-password autocomplete="false">
-              <template #prefix>
-                <el-icon><Avatar /></el-icon>
-              </template>
-            </el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" class="formBtn login-register-form-button">登录</el-button>
+            <el-button>立即登录</el-button>
           </el-form-item>
         </el-form>
       </div>
 
-      <!-- 登陆面板 -->
-      <div :class="['mi-login--switch', { 'is-login': isLogin }]">
-        <div class="mi-login--content">
-          <h1>{{ $route.name == 'feng-login' ? "欢迎回来！" : "您好，欢迎！" }}</h1>
-          <p>
-            {{
-              $route.name == 'feng-login'
-                  ? "如果您还没有账号，请点击下方立即注册按钮进行账号注册"
-                  : "如果您已经注册过账号，请点击下方立即登录按钮进行登录"
-            }}
-          </p>
-          <div class="mi-login--btn" >
-            <el-button v-if="$route.name == 'feng-login'" class="formBtn" >立即注册</el-button>
-            <el-button v-if="$route.name == 'feng-register'" class="formBtn" >立即登录</el-button>
-            <el-button class="formBtn" >返回首页</el-button>
-          </div>
-        </div>
-      </div>
+      <div class="mi-userform__containe--form container-register">
+      <!-- 登陆表 -->
+      <el-form :model="registerForm" class="register__form">
+        <el-form-item>
+          <h1 class="form__title">LOGIN</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="registerForm.username" placeholder="Username" autocomplete="off" clearable></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="registerForm.password" placeholder="Password" autocomplete="off" show-password></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="registerForm.check_password" placeholder="Password" autocomplete="off" show-password></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="registerForm.nick_name" placeholder="Nickname" autocomplete="off" clearable></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button>立即登录</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+    </div>
+      <!-- 登陆面板 -->
+      <div class="container__overlay">
+        <div class="overlay">
+          <div :class="['overlay__panel', ]">
+            <h1>{{ $route.name == 'login' ? "欢迎回来！" : "您好，欢迎！" }}</h1>
+            <p>{{
+                $route.name == 'login'
+                    ? "如果您还没有账号，请点击下方立即注册按钮进行账号注册"
+                    : "如果您已经注册过账号，请点击下方立即登录按钮进行登录"
+              }}</p>
+            <div class="yz-login--btn" >
+              <a-button v-if="route.name == 'login'" >立即注册</a-button>
+              <a-button v-if="route.name == 'register'">立即登录</a-button>
+              <a-button class="formBtn" @click="goTo('/home')">返回首页</a-button>
+            </div>
+          </div>
+      </div>
+
   </div>
 </template>
 
 <style scoped lang="scss">
-@include b(login) {
+@include b(userform) {
   @include bfc;
-  display: flex;
-  justify-content: center;
+  display: grid;
   align-items: center;
+  place-items: center;
   background-image: url("@/assets/images/bgimg.jpg"); /* 背景图片 */
-  background-size: cover;
+  /* 决定背景图像的位置是在视口内固定，或者随着包含它的区块滚动。 */
+  /* https://developer.mozilla.org/zh-CN/docs/Web/CSS/background-attachment */
+  background-attachment: fixed;
+  background-position: center;
   background-repeat: no-repeat; /* 背景图片不重复 */
-  padding: 20px;
-  @include e(main) {
+  @include e(container) {
+    background-color: var(--vt-c-white);
+    border-radius: var(--button-radius);
+    box-shadow: 0 0.9rem 1.7rem rgba(0, 0, 0, 0.25),
+    0 0.7rem 0.7rem rgba(0, 0, 0, 0.22);
+    height: var(--max-height);
+    max-width: var(--max-width);
+    overflow: hidden;
     position: relative;
-    width: 1000px;
-    min-width: 1000px;
-    height: 500px;
-    min-height: 500px;
-    padding: 25px;
-    // box-shadow: 0 15px 50px rgba(0, 0, 0, 0.1);
-    border-radius: 12px;
-    overflow: hidden;
-  }
-  @include m(container) {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    width: 50%;
-    height: 100%;
-    top: 0;
-    // left: calc(100% - 400px);
-    padding: 25px;
-    background-color: #fff;
-    transition: all 1.25s;
-    box-shadow: 0 15px 50px rgba(0, 0, 0, 0.1);
-
-    .loginTitle {
-      text-align: center;
-    }
-  }
-  @include m(switch) {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    top: 0;
-    left: calc(100% - 500px);
-    height: 100%;
-    width: 50%;
-    // padding: 50px;
-    z-index: 200;
-    transition: 1.25s;
-    background: rgba(255, 255, 255, 0.1);
-    overflow: hidden;
-    // box-shadow: 4px 4px 10px #d1d9e6, -4px -4px 10px #f9f9f9;
-    color: #a0a5a8;
-  }
-  @include m(content) {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    position: absolute;
     width: 100%;
-    padding: 50px 55px;
-    transition: 1.25s;
-
-    h1 {
-      font-size: 26px;
-      font-weight: 700;
-      line-height: 3;
-      color: #fff;
+    @include m(form) {
+      height: 100%;
+      position: absolute;
+      top: 0;
+      transition: all 0.6s ease-in-out;
     }
-
-    p {
-      font-size: 14px;
-      letter-spacing: 0.25px;
-      text-align: center;
-      line-height: 1.6;
-    }
-  }
-  @include m(btn) {
-    display: flex;
-    .formBtn:first-child {
-      margin-right: 5px;
+    .right-panel-active {
+      .container-login {
+        transform: translate(100%);
+      }
+      .container-register {
+        animation: show 0.6s;
+        opacity: 1;
+        transform: translate(100%);
+        z-index: 3;
+      }
     }
   }
-
-}
-
-.container-register {
-  z-index: 0;
-  left: calc(100% - 500px);
-  opacity: 0;
 }
 
 .container-login {
-  z-index: 0;
-  left: 0;
+  right: 0;
+  width: 50%;
+  z-index: 2;
 }
-
-.is-txl {
-  left: calc(100% - 500px);
-  transition: 1.25s;
-  transform-origin: right;
-}
-
-.is-z200 {
-  // z-index: 200;
-  transition: 1.25s;
-}
-
-.is-login {
-  left: 0;
-}
-
-.formBtn {
-  margin-top: 30px; /* 按钮顶部与输入框的距离 */
-  width: 150px;
-  height: 30px;
-  color: white; /* 按钮字体颜色 */
-  border: 0; /* 删除按钮边框 */
-  border-radius: 20px; /* 按钮圆角边 */
-  background-image: linear-gradient(to right, #b8cbb8 0%, #b8cbb8 0%, #b465da 0%, #cf6cc9 33%, #ee609c 66%, #ee609c 100%); /* 按钮颜色 */
-}
-.login-register-form {
-  max-width: 300px;
-}
-
-.login-register-form-forgot {
-  float: right;
-}
-
-.login-register-form-button {
-  width: 100%;
+.container-register {
+  right: 0;
+  opacity: 0;
+  width: 50%;
+  z-index: 1;
 }
 </style>
